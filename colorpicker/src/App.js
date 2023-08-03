fc398fe7718bda546822d93f292cf6d26ea8be54
petro-lesapiti <petrolesapiti@gmail.com>
2023-08-03 12:51:01 +0300

    Change text for the black box.

--- colorpicker/src/App.js
+++ colorpicker/src/App.js
@@ -3,12 +3,41 @@ import Colorpicker from './components/Colorrender';
 
 function App() {
   const colors=[
-    { hex: "#FF0000", name: "Red" },
-    { hex: "#00FF00", name: "Green" },
-    { hex: "#0000FF", name: "Blue" },
-  ]
+    {
+        "hex": "#FF0000",
+        "name": "Red",
+    },
+    {
+        "hex": "#00FF00",
+        "name": "Green",
+    },
+    {
+        "hex": "#0000FF",
+        "name": "Blue",
+    },
+    {
+        "hex": "#FFFF00",
+        "name": "Yellow",
+    },
+    {
+      "hex": "#FFFFFF",
+      "name": "White",
+   },
+    {
+        "hex": "#00FFFF",
+        "name": "Cyan",
+    },
+    {
+        "hex": "#FF00FF",
+        "name": "Magenta",
+    },
+    {
+        "hex": "#000000",
+        "name": "Black",
+    },
+]
   return (
-    <div className="App">
+    <div className="mx-8 my-4">
       <Colorpicker colors={colors} />
     </div>
   );
