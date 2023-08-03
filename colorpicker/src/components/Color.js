144771f1621d5f6721b6ae1fda6c25e1f3220532
petro-lesapiti <petrolesapiti@gmail.com>
2023-08-03 12:51:22 +0300

    Add borders for the white box.

--- colorpicker/src/components/Color.js
+++ colorpicker/src/components/Color.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Color = ({hex,name}) => {
+  const textColor = hex === '#000000' ? 'white' : 'black'; 
+  const border= hex === '#FFFFFF' ? '2px solid black': '';
   return (
-    <div style={{background: hex, padding:14}}>
-      <h3>{name}</h3>
+    <div style={{background: hex, padding:14, border: border}}>
+      <h3 style={{color:textColor}}>{name}</h3>
     </div>
   )
 }
