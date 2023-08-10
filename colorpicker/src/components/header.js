d51b28706f3dac2dc649762e7de8fa97f5f6f7e2
petro-lesapiti <petrolesapiti@gmail.com>
2023-08-10 15:02:38 +0300

    Implement word, sentense and paragraph counts

--- colorpicker/src/components/header.js
+++ colorpicker/src/components/header.js
@@ -18,18 +18,19 @@ const Header = () => {
   }
   return (
     <div className='p-4 flex justify-between'>
-        <div className='flex justify-around'>
-            <div href='#' className='flex-auto decoration-slate-50 w-40 uppercase hover:underline'>Text Analyser</div>
+      <div href='#' className='flex-auto decoration-slate-50 w-40 uppercase hover:underline'>Text Analyser</div>
+        <div className='flex justify-around gap-4'>
             <div href='#' className='flex-auto w-14 hover:underline'>About</div>
             <div href='#' className='flex-auto w-20 hover:underline'>Contact Us</div>
-        </div>
-        <button href='#' className='hover:underline' onClick={handleTheme}>
+            <button href='#' className='hover:underline' onClick={handleTheme}>
         {theme === 'dark' ? (
           <FontAwesomeIcon icon={faSun} className='mr-2' />
         ) : (
           <FontAwesomeIcon icon={faMoon} className='mr-2' />
         )}
         </button>
+        </div>
+        
     </div>
   )
 }
