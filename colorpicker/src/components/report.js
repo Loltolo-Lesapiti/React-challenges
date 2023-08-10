95008792738345a06e3b74318302bb459db9e2ca
petro-lesapiti <petrolesapiti@gmail.com>
2023-08-10 15:03:39 +0300

    Include reports props

--- colorpicker/src/components/report.js
+++ colorpicker/src/components/report.js
@@ -1,23 +1,23 @@
 import React from 'react'
 
-const Report = () => {
+const Report = ({words,characters,sentenceCount,paragraphCount} ) => {
   return (
-      <ul className='flex justify-center items-center mt-8'>
-        <li className='w-1/6 h-auto'>
+    <ul className='flex justify-center items-center gap-4'>
+        <li className='w-1/2 h-auto'>
           <p>Words</p>
-          <h3>50</h3>
+          <h3>{words}</h3>
         </li>
-        <li className='w-1/6  h-auto'>
+        <li className='w-1/2  h-auto'>
           <p>Characters</p>
-          <h3>50</h3>
+          <h3>{characters}</h3>
         </li>
-        <li className='w-1/6  h-auto'>
+        <li className='w-1/2  h-auto'>
           <p>Sentences</p>
-          <h3>50</h3>
+          <h3>{sentenceCount}</h3>
         </li>
-        <li className='w-1/6  h-auto'>
+        <li className='w-1/2  h-auto'>
           <p>Paragraphs</p>
-          <h3>50</h3>
+          <h3>{paragraphCount}</h3>
         </li>
       </ul>
   )
