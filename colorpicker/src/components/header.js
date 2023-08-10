763801b4735b4e04df3c9841a61324eba161a69f
petro-lesapiti <petrolesapiti@gmail.com>
2023-08-10 16:46:50 +0300

    Update styling

--- colorpicker/src/components/header.js
+++ colorpicker/src/components/header.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
-
+import { faMoon, faSun} from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Header = () => {
   const [theme, setTheme]=useState('light');
@@ -17,16 +17,16 @@ const Header = () => {
     setTheme(theme==='dark'? 'light': 'dark');
   }
   return (
-    <div className='p-4 flex justify-between'>
-      <div href='#' className='flex-auto decoration-slate-50 w-40 uppercase hover:underline'>Text Analyser</div>
+    <div className='p-4 flex justify-between h-14 bg-gradient-to-r from-#cbd1cc-400 to-blue-400 font-sans hover:font-serif font-bold shadow-2xl'>
+      <div href='#' className='flex-auto decoration-slate-50 w-40 uppercase '>Text Analyser</div>
         <div className='flex justify-around gap-4'>
-            <div href='#' className='flex-auto w-14 hover:underline'>About</div>
-            <div href='#' className='flex-auto w-20 hover:underline'>Contact Us</div>
+            <a href='https://github.com/Loltolo-Lesapiti' target='_blank' rel='noopener noreferrer' className='flex-auto w-14 '><FontAwesomeIcon icon={faGithub} /></a>
+            <a href='https://www.linkedin.com/in/petrolesapitiloltolo/' target='_blank' rel='noopener noreferrer' className='flex-auto w-20 '><FontAwesomeIcon icon={faLinkedin} /></a>
             <button href='#' className='hover:underline' onClick={handleTheme}>
         {theme === 'dark' ? (
-          <FontAwesomeIcon icon={faSun} className='mr-2' />
-        ) : (
           <FontAwesomeIcon icon={faMoon} className='mr-2' />
+        ) : (
+          <FontAwesomeIcon icon={faSun} className='mr-2' />
         )}
         </button>
         </div>
